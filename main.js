5008b018daab9d979b19da1c145c10b1544abaa6
guoconde <guoconde@gmail.com>
2021-11-10 14:05:00 -0300

    Teste

--- main.js
+++ main.js
@@ -2,10 +2,46 @@ const dadosPrincipal = {
     nome: ''
 }
 
+let txt = ''
+const textoLoading = [
+    'Conectando ao servidor.',
+    'Validando suas informações',
+    'Testando Filtros',
+    'Sua esposa sabe que você esta aqui?'
+]
 
 function entrar() {
     dadosPrincipal.nome = document.querySelector('section input').value
 
-    console.log(dadosPrincipal.nome)
+    if (dadosPrincipal.nome != '') {
+        document.querySelector('.invisivel').classList.remove('invisivel')
+        document.querySelector('.login').classList.add('invisivel')
+    } else {
+        alert('Por favor preencha seu nome!')
+    }
 
+    teste()
+
+    // setInterval(() => {
+    //     for (let i = 0; i < textoLoading.length; i++) {
+
+    //         txt.innerHTML = textoLoading[i]
+    //     }
+    // }, 1000)
+
+}
+
+function teste() {
+    setInterval(() => {
+        document.querySelector('.texto').innerHTML = textoLoading[0]
+    }, 1000)
+    setInterval(() => {
+        document.querySelector('.texto').innerHTML = textoLoading[1]
+    }, 2000)
+    setInterval(() => {
+        document.querySelector('.texto').innerHTML = textoLoading[2]
+    }, 3000)
+    setInterval(() => {
+        document.querySelector('.texto').innerHTML = textoLoading[3]
+    }, 4000)
 }
