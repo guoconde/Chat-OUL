389c8d051edb102015effd15b722053888a71183
Gustavo Conde <guoconde@pop-os.localdomain>
2021-11-10 23:41:01 -0300

    adicionando chat

--- main.js
+++ main.js
@@ -5,34 +5,29 @@ const dadosPrincipal = {
 let txt = document.querySelector('.texto').innerHTML
 const textoLoading = [
     'Conectando ao servidor...',
-    'Validando suas informações',
-    'Testando Filtros',
+    'Validando suas informações...',
+    'Testando Filtros...',
     'Sua esposa sabe que você esta aqui?',
     'Não vai carregar com essa sua internet...'
 ]
 
-function entrar() {
+function entrar(el, classe1, classe2) {
     dadosPrincipal.nome = document.querySelector('section input').value
 
     if (dadosPrincipal.nome != '') {
-        document.querySelector('.invisivel').classList.remove('invisivel')
-        document.querySelector('.login').classList.add('invisivel')
+        document.querySelector(`.${classe2}`).classList.remove(`${classe2}`)
+        document.querySelector(`.${classe1}`).classList.add(`${classe2}`)
     } else {
         alert('Por favor preencha seu nome!')
     }
 
-    teste()
+    msgLoading()
 
-    // setInterval(() => {
-    //     for (let i = 0; i < textoLoading.length; i++) {
-
-    //         txt.innerHTML = textoLoading[i]
-    //     }
-    // }, 1000)
+    setTimeout(carregarChat, 100)
 
 }
 
-function teste() {
+function msgLoading() {
     setInterval(() => {
         if(txt == '' || txt == textoLoading[4]) {
             document.querySelector('.texto').innerHTML = textoLoading[0]
@@ -51,4 +46,26 @@ function teste() {
             txt = textoLoading[4]
         }
     }, 1500)
+}
+
+function carregarChat() {
+    document.querySelector('section').classList.add('invisivel')
+    document.querySelector('div.chat').classList.remove('invisivel')
+}
+
+const msg = document.querySelector('div.chat input')
+
+msg.addEventListener('keyup', function(event) {
+    if (event.keyCode == 13) {
+        event.preventDefault()
+        enviarMsg()
+
+        return false
+    }
+})
+
+function enviarMsg() {
+    
+    console.log(msg.value)
+    msg.value = null
 }
